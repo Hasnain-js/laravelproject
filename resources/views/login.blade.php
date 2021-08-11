7a523ad7d1cbeee9ccce85198fd6ea627c8349a2
Hasnain Saeed <ihasnain4@gmail.com>
2021-08-11 07:11:00 -0700

    modify web.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -34,7 +34,7 @@
         <button type="submit" class="btn btn-info float-right btn-lg">login</button>
         </form>
       </div>
-      
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home', function () {
     return view('home');
 });
+// Route::get('/home', function () {
+//     return view('home');
+// });
 Route::get('/login', function () {
     return view('login');
 });
